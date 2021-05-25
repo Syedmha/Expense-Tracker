313f75d2d5e761a63d5466b18381c361382137ef
Syedmha <syedabedi12@gmail.com>
2021-05-25 21:00:17 +0530

    adding JS

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,23 @@
-let exp = document.getElementById('total-exp');
-let expin = parseInt(exp);
+// Getting the elements from HTML
+const disExpense = document.querySelector('#total-exp');
+const inputAmount = document.querySelector('#amount');
+const inputTitle = document.querySelector('#place');
+let selectedCat =  document.querySelector( 'input[name="cat"]:checked'); 
+const form = document.querySelector('#form');
 
-if ( expin > 10) {
-    document.getElementById("quote").innerHTML = "Your Expenses are greater than 10,000/-";
-    console.log('quote');
+// Converting string elemets to float
+let totalExpenses = 0;
+
+
+function logSubmit(e)
+{
+    e.preventDefault();
+    let txtAmount = inputAmount.value;
+    const expense = parseFloat(txtAmount);
+    totalExpenses = totalExpenses + expense;
+    disExpense.textContent = (Math.round(totalExpenses * 100) / 100).toFixed(2); ;
+    
 }
+
+
+form.addEventListener('submit', logSubmit);
