e1c545c3e85535f8043c77d44f5798c12b8ca184
Syedmha <syedabedi12@gmail.com>
2021-05-29 02:26:15 +0530

    update

--- googleauth.js
+++ googleauth.js
@@ -1,16 +1,47 @@
 const signIn = document.querySelector('#sign-in');
 const auth = firebase.auth();
+const currentUser = auth.currentUser;
+
+
 
 const googleAuth = () => {
     let  provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider)
-    .then((result) => {
-        window.location.assign('main.html');
-})
+    .then((result) =>  window.location.assign('main.html'))
 .catch(error => {
     console.error(error);
 })
 }
+
+
+// function updatedb () {
+//     const currentUser = auth.currentUser;
+//     const db = firebase.firestore();
+//     const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+
+//             console.log('yes');
+//             db.collection('users').add({
+//                 uid: currentUser.uid,
+//                 emial: currentUser.email,
+//                 uname: currentUser.displayName,
+//                 image: currentUser.photoURL,
+//                 timestamp: timestamp,
+//               })
+              
+              
+// }
+
+// window.location.assign('main.html')
+
+// async function googleAuth(event) {
+//     event.preventDefault();
+//     let provider = new firebase.auth.GoogleAuthProvider();
+//     try {
+//       await auth.signInWithPopup(provider).then((res) => window.location.assign('main.html'))
+//     } catch (err) {
+//       alert(err.message);
+//     }
+//   }
 
 
 signIn.addEventListener('click', googleAuth);
--- myscripts.js
+++ myscripts.js
@@ -6,7 +6,32 @@ const form = document.querySelector('#form');
 const displayList = document.querySelector('#bottom-records');
 const records = document.querySelector('#records');
 const signout = document.querySelector('#sign-out');
+const userImage = document.querySelector('#userImage');
+const uname = document.querySelector('#h1');
+const helloName = document.querySelector('#welcome');
+const db = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp();
 
+
+const auth = firebase.auth();
+const currentUser = auth.currentUser;
+
+
+auth.onAuthStateChanged((currentUser) => {
+  if (currentUser) {
+    let userName = currentUser.displayName;
+    let firstName = (currentUser.displayName.split(' ')[0]);
+    uname.textContent = userName;
+    helloName.textContent = firstName;
+    userImage.src = currentUser.photoURL;
+    displayTotalExpense();
+    displayItems();
+    // updatedb();
+    //displayName.split(' ')[0]
+  }
+});
+
+ 
 // declaring allExpenses array
 let totalExpenses = 0;
 const allExpenses = [];
@@ -16,54 +41,148 @@ const allExpenses = [];
 function logSubmit(e)
 {  
      e.preventDefault();
-
+     
     // Declaring expenseItem object
     const expenseItem = {};
 
     let txtAmount = inputAmount.value;
     const expense = parseFloat(txtAmount);
-
+    updateTotalExpenses(expense);
     //calcualting totalExpenses
     totalExpenses = totalExpenses + expense;
     disExpense.textContent = (Math.round(totalExpenses * 100) / 100).toFixed(2); 
-    const exp1 = (Math.round(expense * 100) / 100).toFixed(2); 
+    const exp1 = (Math.round(expense * 100) / 100).toFixed(2);
+ 
+    
+
+
+    //Updating Firestore DB
+    const db = firebase.firestore();
+    const uid = auth.currentUser.uid;
+    const name = auth.currentUser.displayName.split(' ')[0];
+    db.collection('users').doc(`${name},${uid}`).collection('ExpenseArray')
+    .add({
+      amountSpent: exp1,
+      item: inputTitle.value ,
+      uid: uid,
+      timestamp: timestamp,
+    });
 
     // Setting values in the object
     expenseItem.amount = exp1;
     expenseItem.title = inputTitle.value;
     expenseItem.cat =  checkButton();
     expenseItem.icon = checkIcon();
+      
+    //generating lists
 
-    // Adding object into the array
-    allExpenses.push(expenseItem);
     
-    // Mapping through the array to display items
-    allExpensesHTML = allExpenses.map(expense => {
-         return  `
-        <div id='bottom-records'>
-    <div id='icon-box'>
-        ${expense.icon}
-    </div>
-    <div id='item-text1'>
-        <h1 id='item-h1'>${expense.title} &nbsp</h1>
-        <p>${expense.cat}</p>
-    </div>
-    <div id='item-text2'>
-        <h1 id='item-price'>~ &#x20b9; ${expense.amount}</h1>
-        <p> 21 May 2021</p>
-    </div>
-    <div id='item-delete'>
-        <i class="fas fa-times"></i>
-    </div>
-    </div>
-    </div>`;
-    });
+
+    
 
     // Joining & Displaying items on the html
-    const joinedallExpenses = allExpensesHTML.join(' ');
-    records.innerHTML = joinedallExpenses; 
+    // const joinedallExpenses = allExpensesHTML.join(' ');
+    // records.innerHTML = generateList(); 
+    // console.log(generateList());
+    
 }
 
+//Updatig total expenses
+function updateTotalExpenses(expense, del) {
+    const db = firebase.firestore();
+    const uid = auth.currentUser.uid;
+    const name = auth.currentUser.displayName.split(' ')[0];
+    if (del) {
+        db.collection('users').doc(`${name},${uid}`).collection('totalexpense').doc('totals')
+        .set({
+          totalExpenses: totalExpenses - expense,
+        });
+    } else {
+        db.collection('users').doc(`${name},${uid}`).collection('totalexpense').doc('totals')
+        .set({
+          totalExpenses: totalExpenses + expense,
+        });
+    }
+  
+    db.collection('users').doc(`${name},${uid}`).collection('totalexpense').doc('totals')
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          totalExpenses = doc.data().totalExpenses;
+          // re rendering the respective element again
+          disExpense.textContent = (Math.round(totalExpenses * 100) / 100).toFixed(2);
+        }
+      });
+  }
+
+
+// Display Total Expense 
+function displayTotalExpense () {
+    const db = firebase.firestore();
+    const uid = auth.currentUser.uid;
+    const name = auth.currentUser.displayName.split(' ')[0];
+
+    db.collection('users').doc(`${name},${uid}`).collection('totalexpense').doc('totals')
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        totalExpenses = doc.data().totalExpenses;
+        // re rendering the respective element again
+        disExpense.textContent = (Math.round(totalExpenses * 100) / 100).toFixed(2);
+    }})
+}
+
+//Displaying list items
+function displayItems() {
+    const db = firebase.firestore();
+    const uid = auth.currentUser.uid;
+    const name = auth.currentUser.displayName.split(' ')[0];
+    db.collection('users').doc(`${name},${uid}`).collection('ExpenseArray')
+      .orderBy("timestamp", "desc")
+      .get()
+      .then((snapshot) => {
+        generateList();
+      });
+    //generateTotalExpenses();
+  }
+
+// Generating lists 
+const generateList = snap_doc => 
+{
+    const db = firebase.firestore();
+    const uid = auth.currentUser.uid;
+    const name = auth.currentUser.displayName.split(' ')[0];
+    db.collection('users').doc(`${name},${uid}`).collection('ExpenseArray')
+      .orderBy("timestamp", "desc").onSnapshot((doc) => {
+        const tr =  
+        `<div id='bottom-records'>
+        <div id='icon-box'>
+            ${checkIcon()}
+        </div>
+        <div id='item-text1'>
+            <h1 id='item-h1'> ${snap_doc.data().item} &nbsp</h1>
+            <p>${checkButton()}</p>
+        </div>
+        <div id='item-text2'>
+            <h1 id='item-price'>~ &#x20b9; ${snap_doc.data().amountSpent}</h1>
+            <p> 21 May 2021</p>
+        </div>
+        <div id='item-delete'>
+            <i class="fas fa-times"></i>
+        </div>
+        </div>
+        </div>`;
+     records.insertAdjacentHTML('beforeend', tr);
+    });
+        
+      
+}
+
+// Joining & Displaying items on the html
+// ${snap_doc.data().item}
+// ${snap_doc.data().amountSpent}
+    // const joinedallExpenses = generateList.join(' ');
+    // records.innerHTML = joinedallExpenses; 
 
 // Funtion to check icons
 function checkIcon() {
@@ -80,7 +199,7 @@ else if(document.getElementById('Shopping').checked) {
 
 
 //Function to check selected item
- function checkButton() {  
+function checkButton() {  
      if(document.getElementById('Food & Beverage').checked) {    
           return  document.getElementById("Food & Beverage").value;
     }   
